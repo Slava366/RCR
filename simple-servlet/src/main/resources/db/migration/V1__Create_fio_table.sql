3c363982df738cb6dc1536864a943c2fed68f47e
Vyacheslav <slava.borgir@gmail.com>
2018-07-07 15:32:51 +0700

    Настройка проекта и подключение зависимостей

---
+++ simple-servlet/src/main/resources/db/migration/V1__Create_fio_table.sql
@@ -0,0 +1,4 @@
+create table users (
+    ID integer auto_increment PRIMARY KEY,
+    FIO varchar(254) UNIQUE NOT NULL
+);
---
+++ simple-servlet/src/main/resources/db/migration/V2__Insert_users_in_fio_table.sql
@@ -0,0 +1,1 @@
+insert into users(fio) values('Тохман Вячеслав Анатольевич');
